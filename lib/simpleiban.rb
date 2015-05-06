753f923b84406b54c4b29a227db5fcd1e759327b
Iñigo gonzalez (exocert) <igponce@gmail.com>
2015-05-06 16:57:34 +0200

    Added simple documentation for a simple class..

--- lib/simpleiban.rb
+++ lib/simpleiban.rb
@@ -3,6 +3,16 @@
 
 class IBAN
 
+   # simple iban cheksum checker
+   # Example:
+   #
+   # foo = 'GB82WEST12345698765432'
+   # foo.is_valid?
+   #
+   # Can also "pretty display" IBAN numbers:
+   # foo.to_s
+
+
    @iban = ''
 
    def initialize (ibanstr)
@@ -12,10 +22,14 @@ class IBAN
    end
 
    def self.check
+     # Checks validity of IBAN, alias for is_valid?
      return is_valid?
    end
 
    def is_valid?
+
+     # Checks if the IBAN checksum is correct and
+     # Good to detect typos in the IBAN
 
      return (@iban.split('').rotate(4).map { |e|
        case e
@@ -28,9 +42,10 @@ class IBAN
 
    end
 
-   # Returns a normalized IBAN string:
-   # alphanimerica characters coverted to uppercase,
    def to_s
+     # Returns a normalized IBAN string: 4-block characters
+     # with uppercase characters when needed.
+
      retval = String.new @iban
      (@iban.length/4).times { |i| retval.insert 4*(@iban.length)/4-4*i, ' ' }
      return retval
