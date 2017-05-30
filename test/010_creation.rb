85baae6709e8c04650c6ebb2d50395f6650afef3
Iñigo González <igponce@gmail.com>
2017-05-30 17:04:33 +0200

    Change to camel_case for readability

--- test/010_creation.rb
+++ test/010_creation.rb
@@ -3,20 +3,20 @@ require 'test/unit'
 
 class TestSimpleIBAN < Test::Unit::TestCase
 
-  def test_detectValidIBANChecksum
+  def test_detect_valid_IBAN_checksum
     a = IBAN.new 'GB82WEST12345698765432' # from wikipedia
     assert a.valid?
   end
 
-  def test_detectInvalidIBANChecksum
+  def test_detect_invalid_IBAN_checksum
     assert !(IBAN.new 'GB00 WEST 000000000000').valid?
   end
 
-  def test_detectValidIBAN_withspaces
+  def test_detect_valid_IBAN_with_spaces
     assert (IBAN.new 'G B8 2WE ST12 34569 876543 2').valid?
   end
 
-  def test_creationDoesNotHaveSideEffectsOnStrings
+  def test_creation_does_not_have_side_effects_on_strings
     str1 = 'GB82 WEST 1234 5698 7654 32'
     str2 = String.new str1
     i = IBAN.new str2
@@ -30,7 +30,7 @@ class TestSimpleIBAN < Test::Unit::TestCase
     assert_match /^([A-Z0-9]{4}\s)+[A-Z0-9]{1,4}?$/, IBAN.new('gb82 wEst 12345698765432').to_s
   end
 
-  def test_IBANWithInvalidCharacters
+  def test_IBAN_with_invalid_characters
     assert_equal false, IBAN.new('GB82WEST|2345698765432').valid?
     assert_equal false, IBAN.new('GB82WEST@2345698765432').valid?
     assert_equal false, IBAN.new('GB82WEST(2345698765432').valid?
